987836ade5b2099b70287148f8823e8f40ce3b90
neeldevopsdeveloper <neel@bluemind.app>
2025-07-22 14:25:21 +0530

    added cloud-run

---
+++ Bluemind-cloudrun-terraform/main.tf
@@ -0,0 +1,86 @@
+resource "google_cloud_run_service" "default" {
+  name     = "bluemind-app"
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = var.image_url
+
+       env {
+         name  = "GCS_BUCKET_NAME"
+         value = "var.gcs_bucket_name"
+       }
+
+        env {
+          name = "GOOGLE_GEMINI_API_KEY"
+          value = var.gemini_key
+        }
+
+        env {
+          name = "SECRET_KEY"
+          value = var.secret_key
+        }
+
+      env {
+        name  = "DB_HOST"
+        value = var.db_connection_name
+      }
+
+      env {
+        name  = "DB_NAME"
+        value = var.db_name
+      } 
+
+      env {
+        name = "DB_PORT"
+        value = "var.db_port"
+      }
+
+      env {
+        name = "DB_USERNAME"
+        value = var.db_user
+      }
+
+      env {
+        name  = "EMAIL_USER"
+        value = var.email_user
+       }
+
+      env {
+        name  = "EMAIL_PASSWORD"
+        value = var.email_password
+       }
+
+     env {
+       name  = "JWT_SECRET"
+       value = var.jwt_secret
+      }
+
+        env {
+          name = "DB_PASSWORD"
+          value_from {
+            secret_key_ref {
+              name = var.db_secret_id
+              key  = "latest"
+            }
+          }
+        }
+      }
+     }
+
+  }
+
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+resource "google_cloud_run_service_iam_member" "public_invoker" {
+  service  = google_cloud_run_service.default.name
+  location = var.region
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
---
+++ Bluemind-cloudrun-terraform/variables.tf
@@ -0,0 +1,62 @@
+variable "project_id" {}
+variable "region" {}
+variable "db_connection_name" {}
+variable "db_secret_id" {}
+
+variable "image_url" {
+  description = "Full Docker image URL for Cloud Run"
+  type        = string
+}
+
+variable "image_tag" {
+  description = "Git commit or image tag to deploy"
+  type        = string
+}
+
+variable "gcs_bucket_name" {
+  type        = string
+  description = "GCS bucket name for uploads"
+}
+
+variable "db_user" {
+  type        = string
+  description = "MySQL username"
+}
+
+variable "db_name" {
+  type        = string
+  description = "MySQL database name"
+}
+variable "email_user" {
+  description = "Email username used for authentication"
+  type        = string
+  sensitive   = true
+}
+
+variable "email_password" {
+  description = "Email password used for authentication"
+  type        = string
+  sensitive   = true
+}
+variable "jwt_secret" {
+  type        = string
+  sensitive   = true
+}
+variable "secret_key" {
+  type        = string
+  description = "Name of the SECRET_KEY in Secret Manager"
+  sensitive   = true
+
+}
+
+variable "gemini_key" {
+  type        = string
+  description = "Name of the Gemini API key in Secret Manager"
+  sensitive   = true
+
+}
+variable "db_port" {
+  type        = number
+  description = "Port number for the database"
+  default     = 3306
+}
